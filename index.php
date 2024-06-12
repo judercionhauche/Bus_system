df8eefdc0a2c2a8b47c55a688ea2bca11462aad5
emmanuel-b-agyei <emmanuel.agyei@ashesi.edu.gh>
2024-06-12 07:59:59 +0000

    login

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+        <!--Link to external CSS stylesheet-->
+        <link rel="stylesheet" href="css/style.css"/>
+        <title>Login/Register Page</title>
+    </head>
+</html>
