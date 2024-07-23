e5b56187d63bef783772447e81cbe49bf9cb1c3b
judercionhauche <judercionhauche@gmail.com>
2024-07-23 00:30:32 +0000

    login session

--- header.php
+++ header.php
@@ -1,25 +1,29 @@
 <?php
-    //include "header.php";
-    define("APPURL","http://localhost/bus_system/")?>
+    // Start the session
+    session_start();
+    
+    // Define the APPURL constant
+    define("APPURL", "http://localhost/bus_system/");
+?>
 <!-- Header -->
 <header id="header">
     <div class="inner">
 
         <!-- Logo -->
-            <a href="index.php" class="logo">
-                <span class="fa fa-bus" style="color:#9E4244"></span> <span class="title" style="color: #9E4244;">Ashesi Bus System</span>
-            </a>
+        <a href="index.php" class="logo">
+            <span class="fa fa-bus" style="color:#9E4244"></span> <span class="title" style="color: #9E4244;">Ashesi Bus System</span>
+        </a>
 
         <!-- Nav -->
-            <nav>
-                <ul>
-                    <?php if (isset($_SESSION['email'])):?>
+        <nav>
+            <ul>
+                <?php if (isset($_SESSION['user_id'])): ?>
                     <li><a href="#menu">Menu</a></li>
-                </ul>
                 <?php else: ?>
                     <li><a href="auth/login.php">Sign in</a></li>
                     <li><a href="about.php">About Us</a></li>
                 <?php endif; ?>
-            </nav>
+            </ul>
+        </nav>
     </div>
 </header>
